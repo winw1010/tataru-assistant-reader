54d1f8f0229498c9a95601a0c380e616f459c4e1
winw1010 <39563714+winw1010@users.noreply.github.com>
2026-04-13 00:21:25 +0800

    Update Program.cs

--- tataru-assistant-reader/Program.cs
+++ tataru-assistant-reader/Program.cs
@@ -67,7 +67,7 @@ namespace tataru_assistant_reader
                         );
                 }
 
-                await Task.Delay(50);
+                await Task.Delay(200);
             }
 
             await SystemFunction.WriteSystemMessage("Stop reading...");
